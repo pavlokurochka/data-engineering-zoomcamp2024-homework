6ef2461b027551d1d98b17908a9517f598002302
Pavlo Kurochka <kurochka@gmail.com>
2024-01-23 21:04:52 -0800

    networking to docker compose finally works

--- 01-docker-terraform/2_docker_sql/pg13_install.sh
+++ 01-docker-terraform/2_docker_sql/pg13_install.sh
@@ -1,27 +1,27 @@
 
-# docker network create pg-network
+# # docker network create pg-network
 
-docker run -it \
-  -e POSTGRES_USER="root" \
-  -e POSTGRES_PASSWORD="root" \
-  -e POSTGRES_DB="ny_taxi" \
-  -v d:/ny_taxi_postgres_data:/var/lib/postgresql/data \
-  -p 5432:5432 \
-  --network=pg-network \
-  --name pg-database \
-  postgres:13   
+# docker run -it \
+#   -e POSTGRES_USER="root" \
+#   -e POSTGRES_PASSWORD="root" \
+#   -e POSTGRES_DB="ny_taxi" \
+#   -v d:/ny_taxi_postgres_data:/var/lib/postgresql/data \
+#   -p 5432:5432 \
+#   --network=pg-network \
+#   --name pg-database \
+#   postgres:13   
 
-  docker run -it \
-  -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
-  -e PGADMIN_DEFAULT_PASSWORD="root" \
-  -p 8080:80 \
-  --network=pg-network \
-  --name pgadmin-2 \
-  dpage/pgadmin4
+#   docker run -it \
+#   -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+#   -e PGADMIN_DEFAULT_PASSWORD="root" \
+#   -p 8080:80 \
+#   --network=pg-network \
+#   --name pgadmin-2 \
+#   dpage/pgadmin4
 
   # in connection use host pg-database 
 
-docker build -t taxi_ingest:v001 .
+# docker build -t taxi_ingest:v001 .
 
   # Run the script with Docker
 
@@ -33,12 +33,12 @@ URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yell
     # --host=pg-database \
 
 docker run -it \
-  --network=pg-network \
+  --network=2_docker_sql_default \
     --name taxi_ingest \
   taxi_ingest:v001 \
     --user=root \
     --password=root \
-    --host=pg-database  \
+    --host=pgdatabase  \
     --port=5432 \
     --db=ny_taxi \
     --table_name=yellow_taxi_trips \
@@ -50,12 +50,12 @@ URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green
     # --host=pg-database \
 
 docker run -it \
-  --network=pg-network \
+  --network=2_docker_sql_default \
     --name taxi_ingest_green \
   taxi_ingest:v001 \
     --user=root \
     --password=root \
-    --host=pg-database  \
+    --host=pgdatabase  \
     --port=5432 \
     --db=ny_taxi \
     --table_name=green_taxi_trips \
@@ -65,16 +65,28 @@ URL="https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv"
   
     # --host=pg-database \
 
+# docker run -it \
+#   --network=pg-network \
+#     --name taxi_ingest_zones \
+#   taxi_ingest:v001 \
+#     --user=root \
+#     --password=root \
+#     --host=pg-database  \
+#     --port=5432 \
+#     --db=ny_taxi \
+#     --table_name=zones \
+#     --url=${URL}
+
+#  docker rm taxi_ingest_zones 
+
 docker run -it \
-  --network=pg-network \
+  --network=2_docker_sql_default \
     --name taxi_ingest_zones \
   taxi_ingest:v001 \
     --user=root \
     --password=root \
-    --host=pg-database  \
+    --host=pgdatabase  \
     --port=5432 \
     --db=ny_taxi \
     --table_name=zones \
     --url=${URL}
-
-#  docker rm taxi_ingest_zones 
